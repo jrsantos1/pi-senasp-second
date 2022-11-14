d6908ab288267d3454f115cfa0da977e276a791a
jrsantos1 <jhonatanjonh@outlook.com>
2022-11-13 23:07:33 -0300

    inserido pagina analytics e contatos

---
+++ static/js/charts/charts.js
@@ -0,0 +1,26 @@
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],}]
+};
+
+function get_config_chart(){
+    const config = {
+        type: 'line',
+        data: data,
+        options: {}
+    };
+    return config
+}
